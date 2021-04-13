bb6f51a72bfb02bfa1d4eb2cbcfa4270cc4f5c19
dlwlgh9412 <dlwlgh9412@gmail.com>
2021-04-14 02:19:17 +0900

    1이 될때까지

--- src/Algorithm/Ex01.java
+++ src/Algorithm/Ex01.java
@@ -1,4 +1,22 @@
 package Algorithm;
 
 public class Ex01 {
+    public static void main(String[] args) {
+        System.out.println(solution(10, 7));
+    }
+
+    private static int solution(int n, int k) {
+        int cnt = 0;
+        while(n != 1) {
+            if(n % k == 0) {
+                n /= k;
+                cnt++;
+            } else {
+                n--;
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
 }
